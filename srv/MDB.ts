452bc6205167d4f2b8c09bb015ce75912cd1d742
vic <uptimevic@gmail.com>
2019-10-13 18:32:52 -0400

    ok

--- srv/MDB.ts
+++ srv/MDB.ts
@@ -1,35 +1,37 @@
 
+const logger = require('tracer').console()
 
+const sqlite3 = require('sqlite3').verbose()
 
-export class MDB {
+import { BaseDBL } from 'mbake/lib/BaseDBL'
 
-    static pool
+export class MDB extends BaseDBL  {
 
     static uuid = require('uuid/v4')
+    
+    protected db
 
     constructor() {
-   
-   
-    }
+        super(null, null)
+        this.db = new sqlite3.cached.Database('file:memdb1?mode=memory&cache=shared')
+    }//()
 
-    schema() { `
-// using C locale
-CREATE UNLOGGED TABLE mon2 (
-    guid        UUID NOT NULL,
-    data        jsonb NOT NULL,
-    dt          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (guid)
-)
-
-CREATE INDEX ihostd ON mon1((data->>'host'), dt) 
-
-`    }
+    async schema() {
+        await this._run(this.db.prepare(`CREATE TABLE mon ( guid PRIMARY KEY NOT NULL, shard_g, load, dt DATETIME DEFAULT CURRENT_TIMESTAMP) `)) 
+   }
 
     async tst() {
+        await this.schema()
 
+       let stmt = this.db.prepare(`INSERT INTO mon(guid, shard_g, load) VALUES( ?,?,?)`)
+        await this._run(stmt, '123', 'us', 3 )
 
-
+        const qry = this.db.prepare(`SELECT * FROM mon `)
+        const rows = await this._qry(qry)
+        console.log(rows)
+        
         console.log('OK')
         process.exit()
     }
 }//()
+
