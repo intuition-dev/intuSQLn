784c05fac754aff4618d2fe0d1ce7532d323e981
vic <uptimevic@gmail.com>
2019-10-19 16:23:51 -0400

    ok

--- src/www/UIBinding.js
+++ src/www/UIBinding.js
@@ -9,17 +9,18 @@ depp.require(['DOM', 'smoothie'], function() {
 }) 
 
 function smoot() {
-   var random = new TimeSeries();
+   var random = new TimeSeries()
    setInterval(function() {
-     random.append(new Date().getTime(), Math.random() * 10000);
-   }, 500)
+     random.append(new Date().getTime(), Math.random() * 1000);
+   }, 1000)
 
    var chart = new SmoothieChart()
    chart.addTimeSeries(random, 
       { strokeStyle: 'rgba(0, 255, 0, 1)',
        fillStyle: 'rgba(0, 255, 0, 0.2)'
        , lineWidth: 4 })
-   chart.streamTo(document.getElementById("chart1"), 500)
+
+   chart.streamTo(document.getElementById("chart1"), 1000)
 
 
 }
