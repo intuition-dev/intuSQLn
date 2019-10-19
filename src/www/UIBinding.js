f437aa86f74a73e42d5f83d981761d878b7e0669
vic <uptimevic@gmail.com>
2019-10-19 16:15:22 -0400

    chart

--- src/www/UIBinding.js
+++ src/www/UIBinding.js
@@ -1,12 +1,28 @@
 
 console.log('UI:')
 
-
 // var vm = new VideModel()
 
-depp.require(['DOM'], function() {
-    console.log('DOM')
+depp.require(['DOM', 'smoothie'], function() {
+   console.log('ready')
+   smoot()
 }) 
+
+function smoot() {
+   var random = new TimeSeries();
+   setInterval(function() {
+     random.append(new Date().getTime(), Math.random() * 10000);
+   }, 500)
+
+   var chart = new SmoothieChart()
+   chart.addTimeSeries(random, 
+      { strokeStyle: 'rgba(0, 255, 0, 1)',
+       fillStyle: 'rgba(0, 255, 0, 0.2)'
+       , lineWidth: 4 })
+   chart.streamTo(document.getElementById("chart1"), 500)
+
+
+}
  
 // sets the states of the view, such as buttons, click enabled/grayed and others
 function pushUIState() {
