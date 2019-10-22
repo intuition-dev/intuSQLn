bdd9b3d6a5ca9f2e9451cc44bce21b0261e1a1a8
vic <uptimevic@gmail.com>
2019-10-22 19:34:43 -0400

    ok

--- rterm/awww/UIBinding.js
+++ rterm/awww/UIBinding.js
@@ -2,29 +2,37 @@
 console.log('UI:')
 
 depp.define({
-   'xterm2': ['https://cdn.jsdelivr.net/npm/xterm@4.1.0/lib/xterm.min.js', 
-      'https://cdn.jsdelivr.net/npm/xterm@4.1.0/css/xterm.css',
-      'https://cdn.jsdelivr.net/npm/xterm-addon-fit@0.2.1/lib/xterm-addon-fit.min.js',
-      'https://cdn.jsdelivr.net/npm/xterm-addon-attach@0.3.0/lib/xterm-addon-attach.min.js'
+   'xterm2': [ 'https://cdn.jsdelivr.net/npm/xterm@4.1.0/lib/xterm.min.js', 
+               'https://cdn.jsdelivr.net/npm/xterm@4.1.0/css/xterm.css',
+               'https://cdn.jsdelivr.net/npm/xterm-addon-fit@0.2.1/lib/xterm-addon-fit.min.js',
+               'https://cdn.jsdelivr.net/npm/xterm-addon-attach@0.3.0/lib/xterm-addon-attach.min.js'
    ]
 
 })
+
 
 depp.require(['poly', 'xterm2', 'html2canvas'], function() {
    console.log('ready')
    setupT()
 }) 
 
+// https://dev.to/davidk01/ptyjs--xtermjs--shell-in-your-browser-1f9c
 function setupT() {
-   Terminal.apply(attach)
-   Terminal.apply(fit)
-   window.term = new Terminal()
+   window.term = new Terminal({cursorStyle: 'bar', cursorBlink: true})
+
+   const fitAddon = new FitAddon()
+   term.loadAddon(fitAddon)
+
    term.open(document.getElementById('terminal'))
 
    // ws ////////////////////
    const url = 'ws://localhost:8080'
    const socket = new WebSocket(url)
-   socket.onopen == function(e) {
+
+   const attachAddon = new AttachAddon(socket)
+   term.loadAddon(attachAddon)
+
+   socket.onopen = function(e) {
       console.log(e)
       term.attach(socket)
    }
@@ -40,7 +48,6 @@ function setupT() {
 }//()
 
 
- 
 // sets the states of the view, such as buttons, click enabled/grayed and others
 function pushUIState() {
 
--- rterm/zagent/PShell.ts
+++ rterm/zagent/PShell.ts
@@ -7,9 +7,7 @@ const WebSocket = require('ws')
 export class PShell {
 
    allowedIP
-
    shell
-
    ws // ws
 
    constructor(port:number, allowedIP? ) {
@@ -19,18 +17,14 @@ export class PShell {
       this.ws.on('connection', this.onCon)
    }
 
-
    onCon(soc, req) {
       // check auth/sec first
       const ip = req.connection.remoteAddress;
-
 
       // after auth/sec continue
       var shell = 'bash'
       this.shell = pty.spwan(shell,[], {
          name: 'xterm-color',
-         cols: 80,
-         rows: 30,
          cwd: process.env.HOME,
          env: process.env 
       })
