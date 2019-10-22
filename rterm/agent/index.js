666f35b5f30ad0da8b31d3cedec82d12746d3040
vic <uptimevic@gmail.com>
2019-10-22 11:28:51 -0400

    ok

--- rterm/agent/index.js
+++ rterm/agent/index.js
@@ -15,3 +15,10 @@ ptyProcess.write('ls\r');
 ptyProcess.resize(100, 40);
 ptyProcess.write('ls\r');
 const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    ws.send('something');
+});
--- rterm/www/UIBinding.js
+++ rterm/www/UIBinding.js
@@ -1,23 +1,31 @@
 
 console.log('UI:')
 
-// var vm = new ViewModel()
+depp.define({
+   'xterm2': ['https://cdn.jsdelivr.net/npm/xterm@4.1.0/lib/xterm.min.js', 
+   'https://cdn.jsdelivr.net/npm/xterm@4.1.0/css/xterm.css',
+   'https://cdn.jsdelivr.net/npm/xterm-addon-fit@0.2.1/lib/xterm-addon-fit.min.js',
+   'https://cdn.jsdelivr.net/npm/xterm-addon-attach@0.3.0/lib/xterm-addon-attach.min.js'
+   ]
 
-depp.require(['poly', 'xterm', 'html2canvas'], function() {
+})
+
+depp.require(['poly', 'xterm2', 'html2canvas'], function() {
    console.log('ready')
    setup()
 }) 
 
 function setup() {
+   // ws ////////////////////
+   let socket = new WebSocket("ws://javascript.info")
+
+
+   if(true) return
    var term = new Terminal();
    term.open(document.getElementById('terminal'));
    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-
-   // ws ////////////////////
-   let socket = new WebSocket("ws://javascript.info")
    
 }
-
 
 function addMessage(message) {
    var text = document.createTextNode(message),
@@ -33,3 +41,5 @@ function addMessage(message) {
 function pushUIState() {
 
 }
+
+// var vm = new ViewModel()
