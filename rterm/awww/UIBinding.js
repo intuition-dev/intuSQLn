db1470aeadb2d333fc99d6dea58c61332fdb9dba
vic <uptimevic@gmail.com>
2019-10-22 19:09:49 -0400

    ok

--- rterm/awww/UIBinding.js
+++ rterm/awww/UIBinding.js
@@ -3,58 +3,42 @@ console.log('UI:')
 
 depp.define({
    'xterm2': ['https://cdn.jsdelivr.net/npm/xterm@4.1.0/lib/xterm.min.js', 
-   'https://cdn.jsdelivr.net/npm/xterm@4.1.0/css/xterm.css',
-   'https://cdn.jsdelivr.net/npm/xterm-addon-fit@0.2.1/lib/xterm-addon-fit.min.js',
-   'https://cdn.jsdelivr.net/npm/xterm-addon-attach@0.3.0/lib/xterm-addon-attach.min.js'
+      'https://cdn.jsdelivr.net/npm/xterm@4.1.0/css/xterm.css',
+      'https://cdn.jsdelivr.net/npm/xterm-addon-fit@0.2.1/lib/xterm-addon-fit.min.js',
+      'https://cdn.jsdelivr.net/npm/xterm-addon-attach@0.3.0/lib/xterm-addon-attach.min.js'
    ]
 
 })
 
 depp.require(['poly', 'xterm2', 'html2canvas'], function() {
    console.log('ready')
-   setup()
+   setupT()
 }) 
 
-function setup() {
-    
+function setupT() {
    Terminal.apply(attach)
    Terminal.apply(fit)
    window.term = new Terminal()
    term.open(document.getElementById('terminal'))
-   term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
 
    // ws ////////////////////
    const url = 'ws://localhost:8080'
    const socket = new WebSocket(url)
-  
-   /*
-   socket.onopen = () => {
-      socket.send('Message From Client') 
+   socket.onopen == function(e) {
+      console.log(e)
+      term.attach(socket)
    }
-   */
 
-   socket.onerror = (error) => {
-      console.log(`WebSocket error: ${error}`)
+   socket.onerror = function(error) {
+      console.log(error)
+   }
+
+   socket.onclose = function(e) {
+      console.log('closed', e)
    }
    
-   /*
-   socket.onmessage = (e) => {
-      console.log('msg', e.data)
-   }
-   */
+}//()
 
-   //combo
-   term.attach(socket)
-}
-
-function addMessage(message) {
-   var text = document.createTextNode(message),
-       el = document.createElement('li'),
-       messages = document.getElementById('messages');
-
-   el.appendChild(text);
-   messages.appendChild(el);
-}
 
  
 // sets the states of the view, such as buttons, click enabled/grayed and others
