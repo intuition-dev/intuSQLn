32b83cd5f97aa1a1cc3ded5583fe5f0142e319a6
vic <uptimevic@gmail.com>
2019-10-21 23:51:14 -0400

    ok

--- rterm/agent/index.ts
+++ rterm/agent/index.ts
@@ -23,4 +23,14 @@ ptyProcess.write('ls\r');
 
 // ws /////////////////////////////////////////////////////////////
 //export {}
-const WebSocket = require('ws');
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message)
+  });
+
+  ws.send('something')
+})
