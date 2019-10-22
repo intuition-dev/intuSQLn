7972f1895e1df7afbe1ece54765a314bc8e0aed1
vic <uptimevic@gmail.com>
2019-10-22 10:54:59 -0400

    ok

--- src/devQ.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const URL = require('url');
-var logger = require('tracer').console();
-const MDB_1 = require("./srv/MDB");
-const m = new MDB_1.MDB();
-m.schema();
-m.showLastPerSecond();
