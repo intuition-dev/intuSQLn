466e936e54a7cb6c5730ade469f2897545daa172
vic <uptimevic@gmail.com>
2019-10-26 18:01:55 -0400

    ok

---
+++ other/load/index.ts
@@ -0,0 +1,36 @@
+
+const URL = require('url')
+
+var logger = require('tracer').console()
+
+// from mbake
+import { BaseRPCMethodHandler, ExpressRPC } from "mbake/lib/Serv"
+
+import { MDB } from "./lib/MDB"
+import { DashHandler } from "./handler/DashHandler"
+
+const m = new MDB()
+m.schema()
+
+m.showLastPerSecond()
+
+const dashSrv = new ExpressRPC()
+dashSrv.makeInstance(['*'])
+
+const handler = new BaseRPCMethodHandler()
+dashSrv.routeRPC('monitor', 'monitor', (req, res) => { 
+
+   const params = URL.parse(req.url, true).query
+   params['ip'] = req.ip // you may need req.ips
+   
+   m.ins(params)
+
+   handler.ret(res, 'OK', 0, 0)
+})
+
+// dash handler
+const dashH = new DashHandler(m)
+dashSrv.routeRPC('dash', 'dash', dashH.handleRPC.bind(dashH) )
+
+
+dashSrv.listen(8888)
