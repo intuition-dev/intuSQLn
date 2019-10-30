80651258d8c60ee2f331f8f251f19906f08c784f
vic <uptimevic@gmail.com>
2019-10-29 20:29:24 -0400

    ok

--- other/geo/lib/GDB.ts
+++ other/geo/lib/GDB.ts
@@ -4,6 +4,8 @@ const log = bunyan.createLogger({src: true, name: "class name"})
 
 import { BaseDBL } from 'mbake/lib/BaseDBL'
 
+const ipInt = require('ip-to-int')
+
 export class GDB extends BaseDBL  {
 
    constructor() {
@@ -21,7 +23,7 @@ export class GDB extends BaseDBL  {
       log.info('.')
       // shard is ip for now, should be geocode
       // dt_stamp is timestamp of last change in GMT
-      this.write(`CREATE TABLE geo( first, last, cont,
+      this.write(`CREATE TABLE geo( fromInt, first, last, cont,
          cou, state, city,
          lat, long
          ) `)
@@ -31,23 +33,32 @@ export class GDB extends BaseDBL  {
    ins(p) {
       //log.info(Date.now(), params)
 
-      this.write(`INSERT INTO geo( first, last, cont,
+      const fromInt = ipInt(p['0'])
+
+      this.write(`INSERT INTO geo( fromInt, first, last, cont,
             cou, state, city, 
             lat, long
          )
             VALUES
-         ( ?,?,?,
+         ( ?,?,?,?.
            ?,?,?,
            ?,?
          )`
          ,
-         p['0'], p['1'], p['2'],
+         fromInt, p['0'], p['1'], p['2'],
          p['3'], p['4'], p['5'],
          p['6'], p['7']
       )
 
    }//()
 
+   /*
+   SELECT longi, lat FROM fast
+   WHERE 68257567 >= frm2 
+   ORDER BY frm2 desc
+   limit 1
+
+   */
    showLastPerSecond(host?) {
 
       const rows = this.read(`SELECT datetime(dt_stamp, 'localtime') as local, * FROM mon
