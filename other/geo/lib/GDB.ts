74c869c54eb329c36ec478ec7d3e16950e899127
vic <uptimevic@gmail.com>
2019-11-03 18:37:29 -0500

    bump

--- other/geo/lib/GDB.ts
+++ other/geo/lib/GDB.ts
@@ -2,7 +2,7 @@
 const bunyan = require('bunyan')
 const bformat = require('bunyan-format')  
 const formatOut = bformat({ outputMode: 'short' })
-const log = bunyan.createLogger({src: true, stream: formatOut, name: "class name"})
+const log = bunyan.createLogger({src: true, stream: formatOut, name: "some name"})
 
 import { BaseDBL } from 'mbake/lib/BaseDBL'
 
--- other/pupp/index.ts
+++ other/pupp/index.ts
@@ -1,29 +1,81 @@
 
+
+ // https://michaljanaszek.com/blog/test-website-performance-with-puppeteer
+
+ // second answer https://stackoverflow.com/questions/52969381/how-can-i-capture-all-network-requests-and-full-response-data-when-loading-a-pag
+
+// https://codecept.io/helpers/Puppeteer#configuration
+// https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md
+
+
+// https://fdalvi.github.io/blog/2018-02-05-puppeteer-network-throttle/
+
 import puppeteer from 'puppeteer'
+
+const bunyan = require('bunyan')
+const bformat = require('bunyan-format')  
+const formatOut = bformat({ outputMode: 'short' })
+const log = bunyan.createLogger({src: true, stream: formatOut, name: "some name"})
 
 
 (async () => {
-   // deploy headless true
-   const browser = await puppeteer.launch({
-      headless: false, 
-      defaultViewport: null,
-      devtools: true
-   })
-   const page = await browser.newPage()
-   const client = await page.target().createCDPSession()
-   await client.send('Network.enable')
+    const browser = await puppeteer.launch();
+    const [page] = await browser.pages();
 
-   await client.send('Network.setRequestInterception', { patterns: [{ 
-         urlPattern: '*', 
-         resourceType: 'Script', 
-         interceptionStage: 'HeadersReceived' 
-         }
-      ]})
+    const results = []; // collects all results
 
-   await page.goto('https://www.ubaycap.com')
-   await page.screenshot({path: 'XxxX.png'})
-  
-   await browser.close()
- })()
+    let paused = false;
+    let pausedRequests = [];
 
- // https://medium.com/@jsoverson/using-chrome-devtools-protocol-with-puppeteer-737a1300bac0
+    const nextRequest = () => { // continue the next request or "unpause"
+        if (pausedRequests.length === 0) {
+            paused = false;
+        } else {
+            // continue first request in "queue"
+            (pausedRequests.shift())(); // calls the request.continue function
+        }
+    };
+
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        if (paused) {
+            pausedRequests.push(() => request.continue());
+        } else {
+            paused = true; // pause, as we are processing a request now
+            request.continue();
+        }
+    });
+
+    page.on('requestfinished', async (request) => {
+        const response = await request.response();
+
+        const responseHeaders = response.headers();
+        let responseBody;
+        if (request.redirectChain().length === 0) {
+            // body can only be access for non-redirect responses
+            responseBody = await response.buffer();
+        }
+
+        const information = {
+            url: request.url(),
+            requestHeaders: request.headers(),
+            requestPostData: request.postData(),
+            responseHeaders: responseHeaders,
+            responseSize: responseHeaders['content-length'],
+            responseBody,
+        };
+        results.push(information);
+
+        nextRequest(); // continue with next request
+    });
+    page.on('requestfailed', (request) => {
+        // handle failed request
+        nextRequest();
+    });
+
+    await page.goto('...', { waitUntil: 'networkidle0' });
+    
+    log.info(results)
+
+    await browser.close();
+})();
