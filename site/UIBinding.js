00e0ef03eb92857fb49fe2694849160e13c02777
vic <uptimevic@gmail.com>
2019-11-04 20:46:27 -0500

    ok

---
+++ site/UIBinding.js
@@ -0,0 +1,14 @@
+
+console.log('UI:')
+
+
+// var vm = new ViewModel()
+
+depp.require(['DOM'], function() {
+    console.log('ready')
+}) 
+ 
+// sets the states of the view, such as buttons, click enabled/grayed and others
+function pushUIState() {
+
+}
--- webGmetrics/gmetrics.ts
+++ webGmetrics/gmetrics.ts
@@ -2,8 +2,28 @@
 
 class __gMetrics {
 
+   private _start = Date.now()
 
-   
+
+   constructor() {
+
+
+      window.addEventListener("error", function (e) {
+         console.log( e.error.message)
+      })
+      window.addEventListener('unhandledrejection', function (e) {
+         console.log( e.reason.message)
+       })
+
+      window.onerror = function(message, source, lineno, colno, error) {
+         console.log(message)
+
+         return true
+      }//
+
+   }//()
+
+
    metrics() {
       var ajax = new XMLHttpRequest()
       ajax.open('GET', 'http://localhost:3000/metrics?a=b')
