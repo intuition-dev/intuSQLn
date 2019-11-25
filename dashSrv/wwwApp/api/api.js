4c9e9f03e0a4c461fef88987e73dcac6da1a5fa8
vic <uptimevic@gmail.com>
2019-11-25 13:54:18 -0500

    ok

--- dashSrv/wwwApp/api/api.js
+++ dashSrv/wwwApp/api/api.js
@@ -1,2 +1,18 @@
 
 console.log('api')
+
+depp.require('RPC', init)
+
+
+function init()  {
+   console.log('rpc')
+
+   const rpc = new httpRPC('http', 'localhost', 8888)
+
+   const pro = rpc.invoke('api', 'pageOne', 'multiply', {a:5, b:2})
+   pro.then(function(resp) {
+   console.log(resp)
+
+})
+
+}
--- dashSrv/wwwApp/dashPg/UIBinding.js
+++ dashSrv/wwwApp/dashPg/UIBinding.js
@@ -2,8 +2,11 @@
 console.log('UI:')
 
 depp.define({
+   'api':'/api/api.js'
 
- 
 })
+
+depp.require('api')
+
 
 // var vm = new ViewModel()
