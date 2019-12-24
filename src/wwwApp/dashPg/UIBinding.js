299a07bdbbaff3c6d66995f0d91f7f166473dbbd
Vic <vic@eml.cc>
2019-12-24 15:35:17 -0500

    d

--- src/wwwApp/dashPg/UIBinding.js
+++ src/wwwApp/dashPg/UIBinding.js
@@ -7,8 +7,3 @@ depp.define({'dash':'/api/DashVM.js'})
 depp.require(['DOM', 'RPC', 'dash'], function() {
     console.log('ready')
 }) 
- 
-// sets the states of the view, such as buttons, click enabled/grayed and others
-function pushState() {
-
-}
