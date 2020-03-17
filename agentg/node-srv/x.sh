ac65dcd48c337c2950bc1f3d17de2dcdd1d3da58
Vic <vic@eml.cc>
2020-03-17 10:23:41 -0400

    d

--- agentg/node-srv/x.sh
+++ agentg/node-srv/x.sh
@@ -2,14 +2,8 @@ clear
 
 tsc
 
-// error 431 : https://stackoverflow.com/questions/32763165/DEV=true node-js-http-get-url-length-limitation
-
 DEV=true node agentg-srv.js
 
-// nohup DEV=true node agentg-srv.js &
+// nohup NODE_ENV=production node agentg-srv.js &
 
-
-// node --max-http-header-size 65535 agentg-srv.js
-
-
-// nohup node --max-http-header-size 65535 agentg-srv.js &
+// Environment=NODE_ENV=production
