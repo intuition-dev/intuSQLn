b905757ca147e1b015acbb4c7abd2fb241986450
Vic <vic@eml.cc>
2020-03-20 16:17:09 -0400

    d

--- JWTaa/db/sdb.ts
+++ JWTaa/db/sdb.ts
@@ -13,66 +13,75 @@ const { Readable } = require('stream')
 export class FDB {
 log:any = new TerseB(this.constructor.name) 
 
-    minioClient
-    bucket 
+minioClient
+bucket 
 
-    constructor() {
-        this.minioClient = new Minio.Client({
-            endPoint: 'ewr1.vultrobjects.com',
-            accessKey: '0X4E06GBGUV1H1C5T0VE',
-            secretKey: 'AdIr5P13vmvXl0IHsh7i1xCWhMafth8XPhxRV8Ju'
-        })
+constructor() {
+    this.minioClient = new Minio.Client({
+        endPoint: 'ewr1.vultrobjects.com',
+        accessKey: '0X4E06GBGUV1H1C5T0VE',
+        secretKey: 'AdIr5P13vmvXl0IHsh7i1xCWhMafth8XPhxRV8Ju'
+    })
 
-        this.bucket = 'aausers'
-    }//()
+    this.bucket = 'aausers'
+}//()
 
-    writeOne(path, data) {  // will overwrite
-        const str = JSON.stringify(data) //encode
-        const fileO =  Readable.from(str) // fileO is a stream, eg   var fileO =  Readable.from("Oh hi") 
+writeOne(fullPath, data) {  // will overwrite
+    const str = JSON.stringify(data) //encode
+    const fileO =  Readable.from(str) // fileO is a stream, eg   const fileO =  Readable.from("Oh hi") 
 
-        const THIZ = this
-        const metaData ={a: 'b'}
-        return new Promise(function(resolve, reject) {
-            THIZ.minioClient.putObject(THIZ.bucket, path, fileO, null, metaData, function(err) {
-                if (err) {
-                    console.log(err)
-                    reject(err)
-                }
-                console.log('saved/uploaded successfully.')
-                resolve()
-              })
-        })
-    }//()
-
-    readOne(path) {
-
-        var size = 0
-        this.minioClient.getObject('mybucket', 'photo.jpg', function(err, dataStream) {
+    const THIZ = this
+    const metaData ={a: 'b'}
+    return new Promise(function(resolve, reject) {
+        THIZ.minioClient.putObject(THIZ.bucket, fullPath, fileO, null, metaData, function(err) {
             if (err) {
-              return console.log(err)
+                THIZ.log.warn(err)
+                reject(err)
+            }
+            THIZ.log.info('saved/uploaded successfully.')
+            resolve()
+        })
+    })//pro
+}//()
+
+readOne(fullPath) {
+    let chunks = []
+    
+    const THIZ = this
+    return new Promise(function(resolve, reject) {
+        THIZ.minioClient.getObject(THIZ.bucket, fullPath, function(err, dataStream) {
+            if (err) {
+                THIZ.log.warn(err)
+                reject(err)
             }
             dataStream.on('data', function(chunk) {
-              size += chunk.length
+                chunks.push(chunk)
             })
             dataStream.on('end', function() {
-              console.log('End. Total size = ' + size)
+                const json = Buffer.concat(chunks).toString()
+
+                const data = JSON.parse(json)
+                THIZ.log.info(data)
+                resolve(data)
+
             })
             dataStream.on('error', function(err) {
-              console.log(err)
+                THIZ.log.warn(err)
+                reject(err)
             })
-          })
+        })
+    })//pro
+}
 
-    }
-
-    list(path) {
-        var stream = this.minioClient.listObjectsV2WithMetadata(this.bucket,path, true,'')
+list(path) {
+    const stream = this.minioClient.listObjectsV2WithMetadata(this.bucket,path, true,'')
+    
+    stream.on('data', function(obj) { 
+        THIZ.log.warn(obj) 
         
-        stream.on('data', function(obj) { 
-            console.log(obj) 
-            
-        } )
+    } )
 
-        stream.on('error', function(err) { console.log(err) } )
-    }
+    stream.on('error', function(err) { THIZ.log.warn(err) } )
+}//()
 
 }//class
