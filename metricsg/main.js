2854157b89b46d982cb130bb88f7e2cf237f98c0
Vic <vic@eml.cc>
2020-04-20 17:53:57 -0400

    d

--- metricsg/main.js
+++ metricsg/main.js
@@ -8,7 +8,7 @@ const MetricsHandler_1 = require("./handler/MetricsHandler");
 const DashHandler_1 = require("./handler/DashHandler");
 const MeDB_1 = require("./db/MeDB");
 const express = require('express');
-const srv = new Serv_1.Serv(['*']);
+const srv = new Serv_1.Serv(['*'], 4 * 1024);
 const db = new MeDB_1.MeDB();
 const mh = new MetricsHandler_1.MetricsHandler(db);
 Serv_1.Serv._expInst.use(express.json({ type: '*/*' }));
@@ -23,7 +23,7 @@ Serv_1.Serv._expInst.post('/log', mh.log);
 //DASH
 const dashH = new DashHandler_1.DashHandler(db);
 srv.routeRPC('api', dashH);
-srv.serveStatic('../wwwApp', 60 * 60, 60);
+srv.serveStatic('./webApp', 60 * 60, 60);
 Serv_1.Serv._expInst.use(function (req, resp, next) {
     log.warn('err, not found', req.originalUrl);
 });
